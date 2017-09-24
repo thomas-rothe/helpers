81153b6e6b8c397c9cc5dc555073739b4569f43b
Michael Käfer <michael.kaefer1@gmx.at>
2017-09-24 16:59:37 +0200

    Create [tasks].show.blade.php

---
+++ laravel/mytemplates/[tasks].show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+
+  <div class="row">
+    <h3>[Task]</h3>
+  </div>
+  
+  <div class="row">
+    <div>{{ [$task]->name }}</div>
+    <div>{{ [$task]->description }}</div>
+  </div>
+  
+@endsection
