3b1c64d09b17a4edf8d528d0a18659b9ddab7eac
Michael Käfer <michael.kaefer1@gmx.at>
2017-09-24 15:52:33 +0200

    Create assets.styles.blade.php

---
+++ laravel/mytemplates/assets.styles.blade.php
@@ -0,0 +1,5 @@
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<link rel="shortcut icon" href="{{ asset('storage/img/kundenportal/ms-icon-70x70.png') }}}">
+<!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">-->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
