8efc6cf367eba2b64d26d6695132f4d25cc9a3e3
Michael Käfer <michael.kaefer1@gmx.at>
2017-09-24 15:12:17 +0200

    Update and rename mytemplates-master.blade.php to mytemplates-layouts.master.blade.php

--- laravel/mytemplates-layouts.master.blade.php
+++ laravel/mytemplates-layouts.master.blade.php
@@ -1,8 +1,18 @@
-<html>
+@include('partials.doctype.html5')
+<html lang="{{ app()->getLocale() }}">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>My Site | @yield('title', 'Home Page')</title>
         
         <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+        
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
     </head>
     <body>
         @section('sidebar')
