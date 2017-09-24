bd19c2e1cd11e7907fc88b9ac3031976e64f88c0
Michael Käfer <michael.kaefer1@gmx.at>
2017-09-24 14:53:31 +0200

    Update mytemplates-master.blade.php

--- laravel/mytemplates-master.blade.php
+++ laravel/mytemplates-master.blade.php
@@ -1,11 +1,18 @@
 <html>
     <head>
         <title>My Site | @yield('title', 'Home Page')</title>
+        
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
     </head>
     <body>
+        @section('sidebar')
+            This is the master sidebar.
+        @show
+        
         <div class="container">
             @yield('content')
         </div>
+        
         @section('footerScripts')
             <script	src="app.js"></script>
         @show
