411894f671a6ae5c5121a86011dd026382f8a583
Michael Käfer <michael.kaefer1@gmx.at>
2017-09-24 15:51:22 +0200

    Update layouts.master.blade.php

--- laravel/mytemplates/layouts.master.blade.php
+++ laravel/mytemplates/layouts.master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>My Site | @yield('title', 'Home Page')</title>
+        <title>{{ config('app.name') }} | @yield('title')</title>
         
         @include('assets.favicon')
         
