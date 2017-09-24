9a4a781fa0489397e945f8733246b75747b18f3c
Michael Käfer <michael.kaefer1@gmx.at>
2017-09-24 22:42:22 +0200

    Create [Task].php

---
+++ laravel/mytemplates/[Task].php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class [Task] extends Model
+{
+    // protected $table         = '[custom_tablename]';
+    // protected $primaryKey    = '[custom_columnname]';
+    // protected $incrementing  = false;
+    // protected $timestamps    = false;
+    // protected $dateFormat    = 'U';
+    // const CREATED_AT         = '[custom_columnname]';
+    // const UPDATED_AT         = '[custom_columnname]';
+    // protected $connection    = '[custom_connectionname]';
+}
