a2b9b5a359a503c4d0d1fbd391bfacb4cbdd800b
Michael Käfer <michael.kaefer1@gmx.at>
2017-10-05 15:35:00 +0200

    Update [Task]Factory.php

--- laravel/mytemplates/[Task]Factory.php
+++ laravel/mytemplates/[Task]Factory.php
@@ -2,9 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Task::class, function (Faker $faker) {
+$factory->define(App\Reusable::class, function (Faker $faker) {
     return [
-		'disposer_id' => 1,
+    	'disposer_id' => function () {
+    	    return factory(App\User::class)->create()->id;
+	    },
 		'type' => $faker->numberBetween(0, 10),
 		'to_pick_at' => $faker->dateTime(),
     ];
