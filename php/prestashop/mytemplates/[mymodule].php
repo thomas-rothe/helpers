9df7c4f3dd9fcdf62274f17f0e9e97c85aa642d3
Michael Käfer <michael.kaefer1@gmx.at>
2017-10-23 13:14:08 +0200

    Create [mymodule].php

---
+++ php/prestashop/mytemplates/[mymodule].php
@@ -0,0 +1,165 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class [Mymodule] extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = '[mymodule]';
+        $this->tab = 'administration';
+        $this->version = '1.0.2';
+        $this->author = '[me]';
+        $this->need_instance = 1;
+
+        $this->bootstrap = true;
+        $this->module_key = '11111111111111222223333';
+
+        parent::__construct();
+
+        $this->displayName = $this->l('[Mymodule]');
+        $this->description = $this->l('[Mymodule description]');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    /***********************************************************************************************************************
+     *  Install and uninstall
+     ***********************************************************************************************************************/
+
+    public function install()
+    {
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            $this->registerHook('displayFooter');
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    /***********************************************************************************************************************
+     *  Configuration form
+     ***********************************************************************************************************************/
+
+    public function getContent()
+    {
+        if (((bool)Tools::isSubmit('submit[Mymodule]')) == true) {
+            $myVar = (string) Tools::getValue('MY_VAR');
+            if (!$myVar || empty($myVar) || !Validate::isPhoneNumber($myVar)) {
+                $message = $this->displayError($this->l('Your configuration value could not be saved. Please put in a valid value.'));
+            } else {
+                $this->postProcess();
+                $message = $this->displayConfirmation($this->l('Settings updated.'));
+            }
+        }
+        return (isset($message) ? $message : '') . $this->renderForm();
+    }
+
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitMwdwhatsappModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm(Configuration::get('MWD_TELEFONNUMMER'))));
+    }
+
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Settings'),
+                    'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('My var'),
+                        'name' => 'MY_VAR',
+                        'desc' => $this->l('Please put in a value.'),
+                        'col' => 5,
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    protected function getConfigFormValues()
+    {
+        return array(
+            'MY_VAR' => Configuration::get('MY_VAR'),
+        );
+    }
+
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /***********************************************************************************************************************
+     *  Hooks for CSS and JS
+     ***********************************************************************************************************************/
+
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path . 'views/js/back.js');
+            $this->context->controller->addCSS($this->_path . 'views/css/back.css');
+        }
+    }
+
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path . '/views/js/front.js');
+        $this->context->controller->addCSS($this->_path . '/views/css/front.css');
+    }
+
+    /***********************************************************************************************************************
+     *  MWD Hooks
+     ***********************************************************************************************************************/
+
+    public function hookDisplayFooter()
+    {
+        $myVar = $this->getConfigFormValues()['MY_VAR'];
+
+        $linkToWhatsappMobile = 'whatsapp://send/?phone=' . $telefonnummer;
+        $linkToWhatsappDesktopAndTablet = 'https://web.whatsapp.com/send?phone=' . $telefonnummer;
+
+        $this->context->smarty->assign(array(
+            'myVar' => $myVar,
+        ));
+        
+        return $this->display(__FILE__, '[mytemplate].tpl');
+    }
+}
