1c4e8f1b9939da1fc0315d5ba7928bb573479279
Michael Käfer <michael.kaefer1@gmx.at>
2017-10-23 13:05:10 +0200

    Create translations.de.php

---
+++ php/prestashop/mytemplates/translations.de.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_8ffc94369e483c4317673c1cb91bb485'] = 'WhatsApp Sticky Link';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_575365cb4e9a3b862f43b97e07a7f75d'] = 'Das Modul erlaubt die Eingabe einer Telefonnummer und verlinkt für Smartphones direkt auf die WhatsApp App, für Desktop auf https://api.whatsapp.com.';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_f4f70727dc34561dfde1a3c529b6205c'] = 'Einstellungen';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_1f8261d17452a959e013666c5df45e07'] = 'Telefonnummer';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_b9c2bab26a7a2a73c6747bdafed8ead2'] = 'Bitte geben Sie eine Telefonnummer im Format \"43664123456789\" ein (ohne Plus, mit Ländervorwahl).';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_c9cc8cce247e49bae79f15173ce97354'] = 'Speichern';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_c695e743dc615abb01863753857630a7'] = 'Ihre Konfiguration konnte nicht gespeichert werden, bitte geben Sie eine Telefonnummer an.';
+$_MODULE['<{mwdwhatsapp}default-bootstrap>mwdwhatsapp_f38f5974cdc23279ffe6d203641a8bdf'] = 'Einstellungen gespeichert.';
