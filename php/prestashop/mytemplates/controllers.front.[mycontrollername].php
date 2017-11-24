317a84555b5c00a4d31adfe6aab0b97e4e126635
Michael Käfer <michael.kaefer1@gmx.at>
2017-11-24 13:47:23 +0100

    Update controllers.front.[mycontrollername].php

--- php/prestashop/mytemplates/controllers.front.[mycontrollername].php
+++ php/prestashop/mytemplates/controllers.front.[mycontrollername].php
@@ -11,10 +11,21 @@ class [mymodulename][mycontrollername]ModuleFrontController extends ModuleFrontC
     {
         parent::initContent();
 
+        // Check if logged in custumer
         if (!$this->context->customer->isLogged() && $this->php_self != 'authentication' && $this->php_self != 'password') {
             Tools::redirect('index.php?controller=authentication?back=my-account');
         }
+        
+        // ..or check if logged in Admin
+		$cookie = new Cookie('psAdmin');
+		if ($cookie->id_employee){
+            // logged in admin
+		}
+		else {
+            // no logged in admin
+		}
 
+        // Do something
         ...
     }
 }
