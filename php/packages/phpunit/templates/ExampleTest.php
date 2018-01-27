0f77e201440679eb6d8b1079e9fa3183eee57fe6
Michael Käfer <michael.kaefer1@gmx.at>
2018-01-27 19:58:50 +0100

    Create ExampleTest.php

---
+++ php/packages/phpunit/templates/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+// a test for a class "Email" would look like:
+
+class EmailTest extends \PHPUnit\Framework\TestCase {
+    public function testSomething()
+    {
+        $this->assertEquals(0, 0);
+    }
+    public function testSomethingElse()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
